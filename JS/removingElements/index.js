6977ea1454bcf73c0abbbbfecfe24ea49339833c
JustSemen <semyn03@mail.ru>
2022-12-15 13:56:50 +0600

    refactor code

--- JS/removingElements/index.js
+++ JS/removingElements/index.js
@@ -1,5 +1,5 @@
 //Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
 
-const removeEveryOther = (arr) => arr.filter((el, index) => index % 2 === 0)
+const removeEveryOther = (arr) => arr.filter((el, index) => !(index % 2))
 
 console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']));
