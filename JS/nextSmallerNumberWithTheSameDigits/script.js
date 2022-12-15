29518b7d1ff336a410dfdfec29c89c06604d1265
JustSemen <semyn03@mail.ru>
2022-12-15 16:11:50 +0600

    refactore

--- JS/nextSmallerNumberWithTheSameDigits/script.js
+++ JS/nextSmallerNumberWithTheSameDigits/script.js
@@ -1,28 +1,11 @@
 //Write a function that takes a positive integer and returns the next smaller positive integer containing the same digits.
 
-let myFunc = num => Number(num);
-
-const isTheSameDigits = (comparedNumber, number) => {
-    let stack = []
-    for (let i = 0; i < number.length; i++) {
-        const indexOfCompared = comparedNumber.indexOf(number[i])
-        if (indexOfCompared === -1) {
-            return false
-        } else {
-            comparedNumber.splice(indexOfCompared, 1)
-        }
-    }
-    return true
-}
+const minify = n => [...`${n}`].sort().join``.replace(/^(0+)([1-9])/, '$2$1');
 
 function nextSmaller(n) {
-    for (let i = n - 1; i > -2; i--) {
-        let comparedNumber = Array.from(String(i), myFunc);
-        let number = Array.from(String(n), myFunc);
-        if (isTheSameDigits(comparedNumber, number))
-            return i
-        if (i < 0) return -1
-    }
+    let min = minify(n)
+    while (--n >= min) if (minify(n) === min) return n
+    return -1
 }
 
 console.log(nextSmaller(414));
