394cb199a97fddd4acaa669107bbfb640d61300c
JustSemen <semyn03@mail.ru>
2022-12-20 13:49:31 +0600

    makeUpperCase

---
+++ SQL/makeUpperCase/request.sql
@@ -0,0 +1,2 @@
+--# write your SQL statement here: you are given a table 'makeuppercase' with column 's', return a table with column 's' and your result in a column named 'res'.
+SELECT s, UPPER(s) as res FROM makeuppercase
