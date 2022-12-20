33d446e010e32b0b8d3ed554f9b6e22f9d7fdd34
JustSemen <semyn03@mail.ru>
2022-12-20 13:22:26 +0600

    add first SQL-request

---
+++ SQL/request.sql
@@ -0,0 +1,6 @@
+--# write your SQL statement here: 
+-- you are given a table 'square' with column 'n'
+-- return a table with this column and your 
+-- result in a column named 'res'
+
+SELECT n, n * n AS res FROM square
