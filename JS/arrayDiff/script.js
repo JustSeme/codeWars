93711758578a61a7bbb25a90ca8c5627c60bc29f
JustSemen <semyn03@mail.ru>
2022-12-20 16:26:43 +0600

    refactore arrayDiff

--- JS/arrayDiff/script.js
+++ JS/arrayDiff/script.js
@@ -1,13 +1,3 @@
-function arrayDiff(a, b) {
-    for (let i = 0; i < a.length; i++) {
-        for (let j = 0; j < b.length + 1; j++) {
-            const indexOfA = b.length === 1 ? a.indexOf(b[0]) : a.indexOf(b[j])
-            if (indexOfA !== -1) {
-                a.splice(indexOfA, 1)
-            }
-        }
-    }
-    return a
-}
+const arrayDiff = (a, b) => a.filter(el => !b.includes(el))
 
 console.log(arrayDiff([3, 3], [3]))
